926b4b7d5d2f4483d58a8b363843df9b20f532e3
steve <stevefrazee123@gmail.com>
2021-11-04 17:42:34 -0400

    finish button_clicker

--- web_fun/button_clicker/js/index.js
+++ web_fun/button_clicker/js/index.js
@@ -1,4 +1,4 @@
-// Set up login button behavior
+// Login button
 function ChangeLoginValue() {
 	if(this.value === "Login") {
 		this.value = "Logout";
@@ -7,15 +7,23 @@ function ChangeLoginValue() {
 		this.value = "Login"
 	}
 }
-
 var login_button = document.querySelector(".header__login-btn");
 login_button.onclick = ChangeLoginValue;
 
-
-// Set up add definition btn behavior
+// Add definition button
 function RemoveItem() {
 	this.remove();
 }
-
 var add_def_btn = document.querySelector(".add-definition-card__btn");
 add_def_btn.onclick = RemoveItem;
+
+// Like buttons
+var like_buttons = document.querySelectorAll(".dictionary-entry__like-btn");
+for(var i in like_buttons) {
+	like_buttons[i].onclick = function() {
+		var num_likes = parseInt(this.value);
+		num_likes += 1;
+		this.value = num_likes + " likes";
+		window.alert("Ninja was liked!");
+	}
+}
