70702475048e64b2fcf53341107cb884dd9fe49e
steve <stevefrazee123@gmail.com>
2021-11-24 18:20:43 -0500

    add bank account assignment

---
+++ python/fundamentals/bank_account.py
@@ -0,0 +1,46 @@
+"""
+Question 1: Is this what the Ninja Bonus was asking for?
+Question 2: Why do we need a classmethod, which accepts the class as argument instead of self,
+            when we can just access the class by typing BankAccount? I guess if we were to change
+            the class name, this would break, whereas using a classmethod won't. Is there any other
+            reason?
+"""
+
+class BankAccount:
+    all_accounts = []
+
+    def __init__(self, int_rate=0.01, balance=0.00):
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.all_accounts.append(self)
+
+    def Deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def Withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def YieldInterest(self):
+        self.balance = self.balance * (1.0 + self.int_rate)
+
+    def DisplayAccountInfo(self):
+        print(self)
+
+    @classmethod
+    def PrintAllAccounts(cls):
+        for account in cls.all_accounts:
+            print(account)
+
+    def __str__(self) -> str:
+        return (f"int_rate: {(self.int_rate * 100.0):.0f}%\n"
+                f"balance:  ${self.balance:.2f}")
+    
+    def __repr__(self) -> str:
+        return str(self)
+
+a1 = BankAccount(0.05, 10000)
+a1.Deposit(200).Deposit(200).Deposit(200).Withdraw(500).YieldInterest()
+a2 = BankAccount(0.05, 10000)
+a2.Deposit(100).Deposit(100).Withdraw(500).Withdraw(500).Withdraw(500).Withdraw(500).YieldInterest()
