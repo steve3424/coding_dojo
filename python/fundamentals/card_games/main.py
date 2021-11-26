b6fa481de47cc25cd31382272a7af82fea44b336
steve <stevefrazee123@gmail.com>
2021-11-26 14:37:27 -0500

    delete commented code in card games

--- python/fundamentals/card_games/main.py
+++ python/fundamentals/card_games/main.py
@@ -52,47 +52,10 @@ def DealCards(deck, players, cards_per_player=None):
             i += 1
 
 def main():
-
     games = [("War", War()), ("Rummy", None), ("Long name hopefully long enough too long", None)]
     game_index = ChooseGame(games) - 1
     current_game_obj = games[game_index][1]
     current_game_obj.GameLoop()
-
-    #cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    #suits = ["\u2660", "\u2663", "\u2665", "\u2666"]
-    #main_deck = Deck(cards, suits)
-    #main_deck.Shuffle(50)
-
-    #players = [Player("Jay"), Player("Ray")]
-
-
-    #game = ChooseGame()
-    #while not game:
-    #    print("\nInput not understood. Try again...")
-    #    game = ChooseGame()
-    
-    #print(f"Dealing for {}")
-    #DealCards(main_deck, players)
-    #round = 1
-    #while game:
-    #    print(f"Round {round}")
-    #    print("Press enter to play")
-    #    print("Press 1 to show scores")
-    #    c = input()
-    #    if c == '':
-    #        played = []
-    #        for p in players:
-    #            card = p.PlayCard()
-    #            played.append((card, p))
-    #            print(f"{p.name} plays {str(card)}")
-    #        print(max(played, key=lambda x: x[0].val))
-    
-    #        round += 1
-    #    elif c == '1':
-    #        for p in players:
-    #            print(f"{p.name}: {len(p.deck.cards)}")
-    #    else:
-    #        game = False
 
 if __name__ == "__main__":
     main()
