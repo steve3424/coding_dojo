e3446bbc5f624fc0d76a8e1c3d8a8d40fd236167
steve <stevefrazee123@gmail.com>
2021-11-30 07:55:07 -0500

    delete commented code

--- python/fundamentals/card_games/main.py
+++ python/fundamentals/card_games/main.py
@@ -3,40 +3,6 @@ from operator import itemgetter
 from menu import RunMenu
 from war import War
 
-#def PrintGameMenu(games):
-#    print("\n******* GAME MENU *******")
-#    print("*  Select a game below  *")
-#    print("*************************")
-#    header_width = 25
-#
-#    for i, game in enumerate(games):
-#        line = f"* [{i + 1}] "
-#        line_len = len(line)
-#        chars_left = header_width - line_len - 2
-#        chars_to_write = len(game[0]) if len(game[0]) <= chars_left else chars_left
-#        line += game[0][0:chars_to_write]
-#        chars_left -= chars_to_write
-#        line += ' ' * chars_left
-#        line += " *"
-#        print(line)
-#    print("* [Ctrl-C] Exit         *")
-#    print("*************************")
-#
-#def ChooseGame(games):
-#    while True:
-#        PrintGameMenu(games)
-#        print("> ", end='')
-#        i = input()
-#        try:
-#            index = int(i)
-#            if index < 1 or index > len(games):
-#                raise ValueError
-#            else:
-#                return index
-#        except ValueError:
-#            print("\nNot an option. Try again")
-#            continue
-
 def DealCards(deck, players, cards_per_player=None):
     num_players = len(players)
     if cards_per_player:
