679a22b8349a6fa580e2c9e6dde16e126fb5aaff
steve <stevefrazee123@gmail.com>
2021-12-23 14:02:57 -0500

    add flask template folder

---
+++ python/flask/TEMPLATE/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def ERR(e):
+    return "Sorry! No response. Try again."
+
+if __name__ == "__main__":
+    app.run(debug=True)
