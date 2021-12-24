8e7de1e75a42ef441ebe0e930627dc08056f3375
steve <stevefrazee123@gmail.com>
2021-12-24 12:24:14 -0500

    update html table

--- python/flask/html_table/static/script.js
+++ python/flask/html_table/static/script.js
@@ -1,9 +1,9 @@
-var table_rows = document.querySelectorAll(".table__row")
-for(var i = 0; i < table_rows.length; ++i) {
-	table_rows[i].onmouseover = function() {
-		this.style.fontWeight = "bold";
-	}
-	table_rows[i].onmouseleave = function() {
-		this.style.fontWeight = "normal";
-	}
-}
+// var table_rows = document.querySelectorAll(".table__row")
+// for(var i = 0; i < table_rows.length; ++i) {
+// 	table_rows[i].onmouseover = function() {
+// 		this.style.color = "red"
+// 	}
+// 	table_rows[i].onmouseleave = function() {
+// 		this.style.color = "black"
+// 	}
+// }
