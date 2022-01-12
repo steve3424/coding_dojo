55dda7cc1ce45b36db478fbca795a8ef9cf3e686
steve <stevefrazee123@gmail.com>
2022-01-11 19:08:07 -0500

    add front end of login/reg page

---
+++ python/flask_mysql/login_and_registration/server.py
@@ -0,0 +1,23 @@
+'''
+This is the main file which starts the server. 
+
+It should reference the controllers since it needs to set up all the routing information.
+
+It should reference the app object to run.
+'''
+
+from flask_app import app
+# TODO: Import controllers so the app.route() can be set up
+# TODO: Import anything from flask that may be needed 
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def ERR(e):
+    return "Sorry! No response. Try again."
+
+if __name__ == "__main__":
+    app.run(debug=True)
